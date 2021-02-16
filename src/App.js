edc4c8b9b99b08e08d65ffc82bebf0a2d316757d
sonalig27 <ganatra.sonali@gmail.com>
2021-02-15 23:02:56 -0500

    made first changes to the create react app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p className='makesonalibold'>Sonali Ganatra</p>
-        <p className='makesonalibold'>Hello</p>
-        <p className='makesonalibold'>Welcome</p>
-        <p className='makesonalibold'>Bye</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>Sonali's first change in the create react app</div>
       </header>
     </div>
   );
